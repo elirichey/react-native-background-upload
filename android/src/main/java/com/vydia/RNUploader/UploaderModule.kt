49d26954fb809168102f4150455b7eeb5dfd000e
Zilla <eli@elirichey.com>
2022-08-09 12:00:01 -0500

    updated the wrong timeout value

--- android/src/main/java/com/vydia/RNUploader/UploaderModule.kt
+++ android/src/main/java/com/vydia/RNUploader/UploaderModule.kt
@@ -63,8 +63,8 @@ class UploaderModule(val reactContext: ReactApplicationContext) : ReactContextBa
     var followRedirects = true
     var followSslRedirects = true
     var retryOnConnectionFailure = true
-    var connectTimeout = 15
-    var writeTimeout = 3000
+    var connectTimeout = 300
+    var writeTimeout = 30
     var readTimeout = 30
     
     //TODO: make 'cache' customizable
